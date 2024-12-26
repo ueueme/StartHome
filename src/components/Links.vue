bb94a656a93878e714cfd6636c17d3b61e73e965
ueueme <mbs@byeks.com>
2024-12-26 13:50:46 +0800

    图标更新

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass,Music,Video, Book, Heartbeat,ShoppingBag , Fire, Laptop ,LaptopCode, Linkedin } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass,Music,GithubAlt,Video,Blogger, Search, Book, Heartbeat,ShoppingBag , Fire, Laptop ,LaptopCode, Linkedin } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -65,7 +65,10 @@ const siteLinksList = computed(() => {
 const siteIcon = {
   Blog,
   Book,
+  Blogger,
+  GithubAlt,
   Cloud,
+  Search,
   ShoppingBag,
   Heartbeat,
   CompactDisc,
